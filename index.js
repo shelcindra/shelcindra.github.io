9e4f10fc9dd113a23435acf79fa478baedd0889d
Shelley Cincotta <48364688+shelcindra@users.noreply.github.com>
2019-10-25 17:22:31 -0400

    Added live typing, etc
    Added live typing from open source typed.js.
    Added CSS styling.

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+
+// Toggle between menu open and close 
 var isOpen = true;
 window.addEventListener("load", function (e) {
     var btn = document.getElementById("mybutton");
@@ -9,5 +11,13 @@ window.addEventListener("load", function (e) {
             mymenu.style.display = "none";
         }
         isOpen = !isOpen;
+    });
+
+    // Add live typing
+    var myElement= document.getElementById("typed");
+    var typed = new Typed(myElement, {
+    stringsElement: '#about',
+    smartBackspace: false,
+    typeSpeed: 40
     });
 });
